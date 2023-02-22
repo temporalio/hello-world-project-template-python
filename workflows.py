c6698ffbec8acbd49d53fa9f1b30146365345ce8
Brian Hogan <bphogan@gmail.com>
2023-02-22 10:30:41 -0600

    Update workflows.py to import activities through sandbox
    Co-authored-by: Chad Retz <chad.retz@gmail.com>

--- workflows.py
+++ workflows.py
@@ -1,7 +1,9 @@
 # @@@SNIPSTART python-project-template-workflows
 from datetime import timedelta
 from temporalio import workflow
-from activities import say_hello
+# Import activity, passing it through the sandbox without reloading the module
+with workflow.unsafe.imports_passed_through():
+    from activities import say_hello
 
 @workflow.defn
 class SayHello:
